55a720f04c504f5bfe9e5b2e545fcef4a8ac7c59
zckoh <zckoh96@gmail.com>
2020-08-15 16:27:19 +0100

    Updates

---
+++ precache-manifest.6153a758bb5b61927b2da58e62bc340a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d80993d6a69066eddba10cbd268a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f5207bbffb06d92618c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e71a9435419668252659",
+    "url": "/static/js/2.6c39958f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c39958f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5207bbffb06d92618c",
+    "url": "/static/js/main.317bfdca.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
